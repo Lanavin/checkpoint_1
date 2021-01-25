27d195e755d9a2ebf64b006597a57451d36588f4
Unknown <unknown@example.com>
2021-01-25 23:19:30 +0300

    commit 3

--- file.py
+++ file.py
@@ -7,3 +7,11 @@ c = int(input())
 print('Сумма =', a+b+c)
 print('Произведение =', a*b*c)
 print('Среднее арифметическое =', (a+b+c)/3)
+
+print('Введите три целых числа:')
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print('Максимальное число =', max(a, b, c))
