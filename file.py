04e28087d45a32eec8061f95e5152a102d40cb93
Unknown <unknown@example.com>
2021-01-25 23:15:51 +0300

    commit 1

---
+++ file.py
@@ -0,0 +1,9 @@
+print('Введите три целых числа')
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print('Сумма =', a+b+c)
+print('Произведение =', a*b*c)
+print('Среднее арифметическое =', (a+b+c)/3)
